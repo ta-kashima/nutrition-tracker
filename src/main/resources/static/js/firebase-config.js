f99c9dfcf95250d70cb33b0e95001a25db091657
takar <takar@takar>
2025-12-25 23:32:31 +0900

    update firebase

---
+++ src/main/resources/static/js/firebase-config.js
@@ -0,0 +1,15 @@
+/**
+ * Firebase設定ファイル
+ */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBe9W-gtv5k8ASTYPW5oCBagcK2Xm-azgM",
+    authDomain: "nutrition-tracker-af04c.firebaseapp.com",
+    projectId: "nutrition-tracker-af04c",
+    storageBucket: "nutrition-tracker-af04c.firebasestorage.app",
+    messagingSenderId: "522963132382",
+    appId: "1:522963132382:web:01cc50e6991b3b1810874f"
+};
+
+// Firebase初期化
+firebase.initializeApp(firebaseConfig);
